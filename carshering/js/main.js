0aff0668af47b91b9607975207bc64e2c3aed42a
Andrey Sorokin <73.anir@gmail.com>
2021-03-20 19:12:52 +0400

    Вёрстка "от 0 до 1" - адаптив на 80%

--- carshering/js/main.js
+++ carshering/js/main.js
@@ -5,6 +5,13 @@ $(function () {
 		dots: true,
 		autoplay: true,
 		fade: true,
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					dots: false,
+				}
+			},]
 	});
 
 	$('.reviews__slider').slick({
@@ -13,6 +20,30 @@ $(function () {
 		slidesToShow: 4,
 		slidesToScroll: 1,
 		autoplay: true,
+		responsive: [
+			{
+				breakpoint: 1141,
+				settings: {
+					slidesToShow: 3,
+				}
+			},
+			{
+				breakpoint: 846,
+				settings: {
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 581,
+				settings: {
+					slidesToShow: 1,
+				}
+			},
+		]
+	});
+
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').toggleClass('menu__list--active')
 	});
 
 });
